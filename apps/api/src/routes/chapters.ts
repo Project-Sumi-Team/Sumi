9b1c6d94b0266540c7891fc5f019f38e1fb316b9
Luna <anamika.ngk@gmail.com>
2026-07-20 19:03:35 +0530

    express deleted

--- apps/api/src/routes/chapters.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as chaptersController from "../controllers/chapters";
-import * as pagesController from "../controllers/pages";
-
-export const router = Router();
-
-router.delete("/:id", chaptersController.deleteChapter);
-router.get("/:chapterId/pages", pagesController.getPages);
-router.post("/:chapterId/pages", pagesController.createPage);
--- apps/api/src/routes/pages.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import * as pagesController from "../controllers/pages";
-
-export const router = Router();
-
-router.delete("/:id", pagesController.deletePage);
-router.get("/:id", pagesController.getPage);
